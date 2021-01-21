393e61abff35a12f3183d0445d51bbcd28485b2a
nina20126 <nina20126@student.hamk.fi>
2021-01-21 21:29:56 +0200

    Lisätty laskin luokka

---
+++ ninan_projekti/ninan_projekti/Laskin.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ninan_projekti
+{
+    public class Laskin
+    {
+        public Laskin()
+        {
+        }
+
+        public int Summa(int luku1, int luku2)
+        {
+            return luku1 + luku2;
+        }
+
+        public int Erotus(int luku1, int luku2)
+        {
+            return luku1 - luku2;
+        }
+    }
+}
