3888146a105c571fb4683a79702f0fc4bdcda7c1
nina20126 <nina20126@student.hamk.fi>
2021-01-21 21:21:08 +0200

    Lisätty laskin luokka

--- ninan_projekti/ninan_projekti/Program.cs
+++ ninan_projekti/ninan_projekti/Program.cs
@@ -10,6 +10,13 @@ namespace ninan_projekti
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello Laskin!");
+
+            var laskin = new Laskin();
+
+            Console.WriteLine("Tulosta lukujen 5 ja 4 summa.");
+            var summa = laskin.Summa(5, 4);
+            Console.WriteLine(summa);
         }
     }
 }
