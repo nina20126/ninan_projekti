10b210f2790ede1052fa226c297bffd4db7e6dd6
nina20126 <nina20126@student.hamk.fi>
2021-01-21 20:48:29 +0200

    Konsolisovelluksen ensimmäinen versio

---
+++ ninan_projekti/ninan_projekti/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ninan_projekti
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
